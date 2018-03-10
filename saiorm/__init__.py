1e73059749ce83ec1fd8b0d34ea4c8ca5f53fe98
xxx <xxx_828@163.com>
2018-03-10 22:49:46 +0800

    modify bridge

--- saiorm/__init__.py
+++ saiorm/__init__.py
@@ -1,6 +1,8 @@
-from .MySQL import *
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
 
 
 def init(driver="MySQL", table_name_prefix=""):
     if driver.lower() == "mysql":
+        from .MySQL import CoherentDB
         return CoherentDB(table_name_prefix=table_name_prefix)
