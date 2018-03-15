e4e565af9dad959ea99e55091e5e86c04aa952b5
xxx <xxx_828@163.com>
2018-03-15 22:30:26 +0800

    secret PositionDB

--- saiorm/MySQL.py
+++ saiorm/MySQL.py
@@ -3,6 +3,7 @@
 """
 Support MySQL
 """
+import ast
 import time
 from pymysql import cursors
 from pymysql import connect
@@ -162,3 +163,234 @@ class Connection(object):
 class ChainDB(base.ChainDB):
     def connect(self, config_dict=None):
         self.db = Connection(**config_dict)
+
+
+class PositionDB(Connection):
+    """
+    Implement database operation by position argument.
+
+    Table name prefix placeholder in initialization is prefix_sign, defaults to "###".
+
+    **Method argument position**::
+
+    select method: table, field, condition
+    insert method: table, field
+    update method: table, field, condition
+    delete method: table, condition
+    count method: table, field, condition
+    alter method: table, condition
+
+    **Argument**
+    :table: str,table name（not include prefix）
+    :field: [list|tuple|str],field name.
+        If call native mysql function,should use literal dict with one key-value,like:
+        'username, nickname, {"reg_time": "now()"}'
+    :condition: other condition,including WHERE, ORDER BY, LIMIT, GROUP BY etc.
+
+    **Multi line**
+    param same as torndb
+
+    **return**
+    insert, update, delete and their many function returns tuple with lastrowid and rowcount.
+
+    **Usage**::
+
+    - initialization:
+        PositionDB("127.0.0.1", 3306, "database", "user", "password", "table_name_prefix)
+
+    - call mysql function without param：
+    >>>insert("user","username,nickname,{'reg_time':'now()'}", username, nickname)
+    will transform to:
+
+    .. code:: sql
+
+        INSERT INTO user (username, nickname, reg_time) value (%s,%s,%s,now())', username, nickname
+
+    - call mysql function with param(use %s as placeholder)：
+    >>>insert('log',"user_id,{'ip': 'inet_aton(%s)'},uri,action_no",user_id,ip, uri,action_no)
+    or
+    >>>insert('log',"user_id,ip=inet_aton(%s),uri,action_no",user_id,ip, uri,action_no)
+    will transform to
+
+    .. code:: sql
+
+        INSERT INTO f4isw_log (user_id,ip,uri,action_no) VALUE (%s,inet_aton(%s), %s,%s)
+
+    - use select or count to count
+    >>>select("user", "COUNT(id) AS rows_count", "")
+    equals to:
+    >>>count("user", "id", "")
+    """
+
+    def __init__(self, host, port, database, user=None, password=None,
+                 max_idle_time=7 * 3600, connect_timeout=60, time_zone="+0:00",
+                 prefix="", prefix_sign="###", grace_result=True):
+        super().__init__(host, port, database, user, password,
+                         max_idle_time, connect_timeout, time_zone)
+        self.prefix = prefix  # table name prefix
+        self.prefix_sign = prefix_sign  # 替换表前缀的字符
+        self.grace_result = grace_result
+
+    def _execute(self, cursor, query, parameters, kwparameters):
+        if self.prefix_sign in query:
+            query = query.replace(self.prefix_sign, self.prefix)
+
+        super()._execute(cursor, query, parameters, kwparameters)
+
+    def query(self, query, *parameters, **kwparameters):
+        """Returns a row list for the given query and parameters."""
+        cursor = self._cursor()
+        try:
+            self._execute(cursor, query, parameters, kwparameters)
+            column_names = [d[0] for d in cursor.description]
+
+            if self.grace_result:
+                return [GraceDict(zip(column_names, row)) for row in cursor]
+            else:
+                return [zip(column_names, row) for row in cursor]
+        finally:
+            cursor.close()
+
+    def execute_both(self, query, *parameters, **kwparameters):
+        """return lastrowid and rowcount"""
+        cursor = self._cursor()
+        try:
+            self._execute(cursor, query, parameters, kwparameters)
+            return cursor.lastrowid, cursor.rowcount
+        finally:
+            cursor.close()
+
+    def executemany_both(self, query, parameters):
+        """return lastrowid and rowcount"""
+        cursor = self._cursor()
+        try:
+            cursor.executemany(query, parameters)
+            return cursor.lastrowid, cursor.rowcount
+        finally:
+            cursor.close()
+
+    def mk_insert_query(self, table, field, many=False):
+        """
+        :param many: bool,the sign of inserting many data in one line
+        """
+        table = self.prefix + table
+        if isinstance(field, str):
+            field = [i.strip() for i in field.split(",")]
+        field_str = ""
+        value_str = ""
+
+        for i in field:
+            if i.startswith("{") and i.endswith("}"):
+                # native mysql function
+                ei = ast.literal_eval(i)
+                keys = list(ei.keys())
+                values = list(ei.values())
+                field_str += (keys[0] + ",")
+                value_str += (values[0] + ",")
+            elif "=" in i:  # native mysql function
+                k, v = i.split("=")
+                field_str += (k + ",")
+                value_str += (v + ",")
+            else:
+                field_str += (i + ",")
+                value_str += "%s,"
+        if not many:
+            query = "INSERT INTO {} ({}) VALUE {}".format(table, field_str[:-1], value_str)
+        else:
+            query = "INSERT INTO {} ({}) VALUES {}".format(table, field_str[:-1], value_str)
+        return query
+
+    def mk_delete_query(self, table, condition):
+        # 生成 delete 语句
+        table = self.prefix + table
+        query = "DELETE FROM " + table + " " + condition
+        return query
+
+    def mk_update_query(self, table, field, condition):
+        # 生成 update 语句
+        table = self.prefix + table
+        if isinstance(field, str):
+            field = [i.strip() for i in field.split(",")]
+
+        field_str = ""
+        for i in field:
+            if i.startswith("{") and i.endswith("}"):
+                # native mysql function
+                i = ast.literal_eval(i)
+                iks = list(i.keys())
+                ivs = list(i.values())
+                field_str += (iks[0] + "=" + str(ivs[0]) + ", ")
+            elif "=" in i:  # native mysql function
+                k, v = i.split("=")
+                field_str += (k + "=" + v)
+            else:
+                field_str += (i + "=%s, ")
+
+        query = "UPDATE {} SET {} {}".format(table, field_str[:-2], condition)
+        return query
+
+    def insert(self, table, field, *parameters, **kwparameters):
+        """
+
+        :return: tuple,lastrowid and rowcount
+        """
+        query = self.mk_insert_query(table, field, many=True)
+        return self.execute_both(query, *parameters, **kwparameters)
+
+    def insert_many(self, table, field, *parameters, **kwparameters):
+        """
+
+        :return: tuple,lastrowid and rowcount
+        """
+        query = self.mk_insert_query(table, field, many=True)
+        return self.executemany_both(query, *parameters, **kwparameters)
+
+    def delete(self, table, condition, *parameters, **kwparameters):
+        """
+
+        :return: tuple,lastrowid and rowcount
+        """
+        query = self.mk_delete_query(table, condition)
+        return self.execute_both(query, *parameters, **kwparameters)
+
+    def update(self, table, field, condition, *parameters, **kwparameters):
+        """
+
+        :return: tuple,lastrowid and rowcount
+        """
+        query = self.mk_update_query(table, field, condition)
+        return self.execute_both(query, *parameters, **kwparameters)
+
+    def select(self, table, field, condition, *parameters, **kwparameters):
+        """
+        return all data with the conditions
+
+        :return: list
+        """
+        table = self.prefix + table
+        query = "SELECT " + field + " FROM " + table + " " + condition
+        return self.query(query, *parameters, **kwparameters)
+
+    def get(self, table, field, condition, *parameters, **kwparameters):
+        """
+        return the latest line of data with the conditions
+
+        :return: dict
+        """
+        data = self.select(table, field, condition, *parameters, **kwparameters)
+        return data[0] if data else []
+
+    def count(self, table, field, condition, *parameters, **kwparameters):
+        """
+        The number of data with the conditions
+
+        :return: int
+        """
+        table = self.prefix + table
+        field = 'COUNT(' + field + ') AS rows_count'
+        query = "SELECT " + field + " FROM " + table + " " + condition
+        rows_count = self.query(query, *parameters, **kwparameters)
+        if rows_count:
+            return int(rows_count[0]["rows_count"])
+        else:
+            return 0
--- saiorm/__init__.py
+++ saiorm/__init__.py
@@ -6,6 +6,9 @@ def init(driver="MySQL", **kwargs):
     if driver.lower() == "mysql":
         from .MySQL import ChainDB
         return ChainDB(**kwargs)
+    if driver.lower() == "mysql_position":
+        from .MySQL import PositionDB
+        return PositionDB
     elif driver.lower() == "postgresql":
         from .PostgreSQL import ChainDB
         return ChainDB(**kwargs)
