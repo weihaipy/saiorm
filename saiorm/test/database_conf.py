f0969b3c67828f575c9294557b346217b5cfec49
xiazhujie <xiazhujie@126.com>
2020-02-29 21:11:02 +0800

    添加测试代码;

---
+++ saiorm/test/database_conf.py
@@ -0,0 +1,20 @@
+MySQL = {
+    "table_name_prefix": "prefix_",
+    "host": "127.0.0.1",
+    "port": "3306",
+    "database": "saiorm_test",
+    "user": "saiorm_test",
+    "password": "saiorm_test"
+}
+PosgresSQL = {
+
+}
+SQLServer = {
+
+}
+SQLite = {
+
+}
+MongoDB = {
+
+}
